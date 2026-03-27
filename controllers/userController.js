59306d1455714f04f3931c6955b6028b27785999
Leonardo <leofronchi@outlook.com>
2026-03-27 14:16:13 -0300

    mudando configuração de cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,8 +18,8 @@ const signIn = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
